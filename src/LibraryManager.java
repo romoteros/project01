bfc3fe962c77207802126de4c776067d6d27c47c
Romoteros <romoteros@gmail.com>
2026-03-04 22:26:04 +0100

    Primera refactorización (cambiar variables a inglés)

---
+++ src/LibraryManager.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class LibraryManager {
+
+    private List<String> books = new ArrayList<>();
+    private List<String> users = new ArrayList<>();
+    
+    public void procesarPrestamo(String user, String book, int day) {
+	    if (users.contains(user) && books.contains(book)) {
+	        System.out.println("Verificando disponibilidad...");
+	        boolean isAvailable = true;
+	        if (isAvailable) {
+	            System.out.println("Prestamo realizado");
+	            books.remove(book);
+
+	            // Calcular fecha de devolución
+	            Calendar cal = Calendar.getInstance();
+	            cal.add(Calendar.DAY_OF_MONTH, day);
+	            System.out.println("Fecha devolucion: " + cal.getTime());
+	        }
+	    }
+	}
+}
